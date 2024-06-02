22614b2e27e98026449f23471fa2bbac570a1e8f
Julio Neto <julioramos3@gmail.com>
2024-06-02 10:55:20 -0300

    Praticas e estudos sobre variaveis metodos e operadores

---
+++ java-Variaveis-Metodos/src/operadores.java
@@ -0,0 +1,26 @@
+public class operadores {
+
+    public static void main(String[] args) {
+       // String nomecompleto = "LINGUAGEM"+"JAVA";
+
+        String concatenacao = "?";
+
+        concatenacao = 2+2+5+"25";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 2+"15"+3;
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+ 2 + 3;
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+(2 + 3);
+
+        System.out.println(concatenacao);
+        
+
+    }
+}
---
+++ java-Variaveis-Metodos/src/tiposVariaveis.java
@@ -0,0 +1,24 @@
+public class tiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        System.out.println("teste de escrita");
+
+        // tipos de variaveis primitivos
+        // String representa texto
+
+        double salarioMinimo = 2500;
+
+        int numero = 5;
+
+        double salarioFracionado = 2500.66;
+
+        numero = 15;
+
+        String string = "teste de texto com variavel string";
+
+        final int paciencia = 2;
+
+        boolean consideraJovem = false; 
+
+        char sexo = 'M';
+    }
+}
