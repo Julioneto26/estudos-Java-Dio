5c6fe26ff5414a9512ca7341e58bc33e9db7a780
Julio Neto <julioramos3@gmail.com>
2024-07-23 21:40:34 -0300

    complementação do teste de caixa eletronico if e else composto

--- java-Variaveis-Metodos/src/EstruturasCondicionais.java
+++ java-Variaveis-Metodos/src/EstruturasCondicionais.java
@@ -8,25 +8,69 @@ public class EstruturasCondicionais{
 
         double saldo = 1500;
 
-        System.out.println("O que deseja? 1: Mostra Saldo 2: Fazer um saque");
+        System.out.println("O que deseja?");
+        System.out.println("1 : Ver Saldo");
+        System.out.println("2: Fazer um Saque");
 
         int iniciar = scanner.nextInt();
 
         if (iniciar==1) {
-            System.out.println(saldo);
+            System.out.println("Seu saldo atual é: R$" + saldo);
+            System.out.println("Quer continuar para o saque?");
+            System.out.println("1: Não");
+            System.out.println("2: Sim");
+
+            int iniciar2 = scanner.nextInt();
+
+                if (iniciar2 == 1) {
+
+                    System.out.println("Obrigado Por escolher Nosso Banco. Tenha um Bom dia!");
+                    
+                }
+
+                else {
+
+                    System.out.println("Qual o valor do Saque?");
+
+
+                }
+
 
         }
 
-        System.out.println("Qual o valor do Saque?");
+        else if (iniciar != 1 && iniciar != 2) {
+            
+            System.out.println("Por favor selecione numero 1 ou 2 ");
+
+        }
+        else {
+
+            System.out.println("Qual o valor do Saque?");
+
+        }
+
+        
 
         double valorSaque = scanner.nextDouble();
         if (valorSaque>saldo) {
 
-            System.out.println("Saldo Insuficiente! Este é o seu saldo atual:" + saldo);
+            System.out.println("Saldo Insuficiente! Este é o seu saldo atual: R$" + saldo);
             
         }
+        else {
 
-        System.out.println("Saque sendo processado!");
+            System.out.println("Saque sendo processado!");
+
+            saldo = saldo - valorSaque;
+
+            System.out.println("Novo saldo: R$" + saldo);
+
+            System.out.println("Obrigado por escolher Nosso banco");
+
+
+        }
+
+        
 
     }
 
